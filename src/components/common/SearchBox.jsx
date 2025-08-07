c84a952a64b3e39825b62f8d7ad49eb969f7f762
tquang2410 <justicepencil@gmail.com>
2025-08-07 12:18:23 +0700

    Use debounce for search function

--- src/components/common/SearchBox.jsx
+++ src/components/common/SearchBox.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
-
+import debounce from 'lodash.debounce';
 
 const SearchBox = ({ searchTerm, onSearch }) => {
+    const debouncedOnSearch = useRef(debounce((value) => {
+        onSearch(value);
+    }, 500)).current;
     return (
         <div className="search-box">
             <input
                 type="text"
                 placeholder="Search..."
-                value={searchTerm} // Gán giá trị từ Redux
-                onChange={(e) => onSearch(e.target.value)} // Cập nhật giá trị lên Redux
+                defaultValue={searchTerm} // Gán giá trị từ Redux
+                onChange={(e) => debouncedOnSearch(e.target.value)} // Cập nhật giá trị lên Redux
             />
-            <button type="submit">Search</button>
         </div>
     );
 }
